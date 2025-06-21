0d8f1b9646c01b90701f6da90a842c052e8690ea
Sambolec <119959747+Sambolec@users.noreply.github.com>
2025-06-21 12:02:12 +0200

    Update market.js

--- routes/market.js
+++ routes/market.js
@@ -2,6 +2,8 @@ const express = require('express');
 const Card = require('../models/Card');
 const router = express.Router();
 
+// ok; moga bi te pitat da objasniš ča ova funkcija dela i da nešto izmjeniš u njoj
+// ali samo iterira kroz karte i nasumično im mjenja cijenu, pa ni preteško
 router.get('/simulate', async (req, res) => {
   const cards = await Card.find();
   for (let card of cards) {
